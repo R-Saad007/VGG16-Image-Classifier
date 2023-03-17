c53a9726d26869c0a916d803a1a1b0564e82628b
Rana Saad Imran <22100018@lums.edu.pk>
2023-03-17 12:38:35 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,30 @@
+from libraries import *
+from normalize_load_data import test_dataloader
+from vgg import *
+
+# Test function
+def test(save_flag):
+    correct = 0
+    accuracy = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_dataloader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = VGG16_net(images)
+            outputs = outputs.to(device)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    # accuracy
+    accuracy = (correct / total) * 100
+    print('Finished Testing')
+    print('Accuracy on the 10000 test images: %.2f %%' % accuracy)
+    if save_flag:
+        # to save the CNN model
+        PATH = './cifar_net.pth'
+        torch.save(VGG16_net.state_dict(), PATH)
+        print('Model Saved')
+    return accuracy
